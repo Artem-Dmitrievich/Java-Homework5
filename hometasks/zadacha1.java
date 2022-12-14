26c71829f01db5e8d7d8778c036ec0f6c91e5bc1
kozlov artem <artemko1987@yandex.ru>
2022-12-15 00:56:54 +0300

    hometasks

--- hometasks/zadacha1.java
+++ hometasks/zadacha1.java
@@ -9,21 +9,21 @@ public class zadacha1 {
     public static void main(String[] args) {
 
         Map<String, String> bookFone = new HashMap<>();    
-        bookFone.put("Иванов И.И.", "2-23-23");
+        bookFone.put("Самута И.Г.", "2-23-23");
         bookFone.put("Звягтин А.А.", "3-23-63");
         bookFone.put("Приввалов Д.Н.", "2-76-53");
         bookFone.put("Смирнов К.А.", "3 -15-78");
-        bookFone.put("Любомиров С.С.", "2-23-89, 2-43-53");
+        bookFone.put("Фадеев С.С.", "2-23-89, 2-43-53");
         bookFone.put("Горшков П.К.", "2-15-13");
         bookFone.put("Лобанов Г.О.", "3-78-99, 2-29-00");
-        System.out.println(bookFone.get("Иванов И.И."));
+        System.out.println(bookFone.get("Самута И.Г."));
         System.out.println(bookFone.get("Лобанов Г.О."));
 
-        addBookFone(bookFone, "Костров Б.Е.", "2-02-45");
+        addBookFone(bookFone, "Козлов А.Д.", "2-02-45");
 
-        System.out.println(bookFone.get("Костров Б.Е."));
+        System.out.println(bookFone.get("Козлов А.Д."));
 
-        deleteBookFone(bookFone, "Иванов И.И.");
+        deleteBookFone(bookFone, "Самута И.Г.");
 
         System.out.println(bookFone);
     }
